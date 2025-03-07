419d275d6a4ed79d5a89d1d39549544ede456abf
Raumsegler1 <113139832+Raumsegler1@users.noreply.github.com.>
2025-03-07 21:54:37 +0100

    ads Flake.nix

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
+
+    # home-manager = {
+    #   url = "github:nix-community/home-manager";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.magnetar = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./configuration.nix
+        # inputs.home-manager.nixosModules.default
+      ];
+    };
+  };
+}
