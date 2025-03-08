53ba91927637e964fbc4fc27e2633b569a44aa8e
Raumsegler1 <113139832+Raumsegler1@users.noreply.github.com.>
2025-03-08 16:03:22 +0100

    fix stuff and add nh

--- configuration.nix
+++ configuration.nix
@@ -212,6 +212,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+  nh
   wget
   chromium
   thunderbird
@@ -220,6 +221,10 @@
   btop
   git
   ];
+
+  environment.sessionVariables = {
+    FLAKE = "/etc/nixos#username";
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
--- home.nix
+++ home.nix
@@ -23,6 +23,7 @@
       enable = true;
       settings = {
         theme_background = false; #any btop settings break the rebuild for whatever reason
+        color_theme = "kyli0x";
       };
     };
     kitty = {
@@ -31,6 +32,29 @@
         package = pkgs.nerdfonts;
         name = "JetBrainsMonoNerdFont, Iosevka Nerd Font, Noto Nerd Font";
         size = 10;
+      };
+      settings = {
+        window_margin_width = 0;
+        window_padding_width = 4;
+        confirm_os_windows_close = 0;
+
+        background_opacity = 1;
+        sync_to_monitor = true;
+        tab_bar_edge = "bottom";
+        tab_bar_style = "fade";
+
+        cursor_shape = "beam";
+        cursor_beam_thickness = "4.5";
+        curso_blink_interval = 0;
+
+        detect_urls = true;
+        url_color = "#0087bd";
+        url_style = "curly";
+
+        enable_audio_bell = false;
+
+        scrollback_lines = 1500;
+        wheel_scroll_multiplier = "10.0";
       };
     };
   };
@@ -67,7 +91,9 @@
 
   home.sessionVariables = {
     EDITOR = "vscodium";
-    SHELL = "kitty";
+    TERMINAL = "kitty";
+    SHELL = "fish";
+    BROWSER = "chromium";
   };
 
   # Let Home Manager install and manage itself.
