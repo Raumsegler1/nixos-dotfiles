040321c59748135fa3cab9283ce21e059f6b2837
Raumsegler1 <113139832+Raumsegler1@users.noreply.github.com.>
2025-03-08 14:40:09 +0100

    configure stuff

--- home.nix
+++ home.nix
@@ -1,19 +1,12 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
+  imports = [
+    ./modules/myModule.nix
+  ];
+  # Home Manager needs a bit of information about you and the paths it should manage.
   home.username = "raumsegler";
   home.homeDirectory = "/home/raumsegler";
-
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.11"; # Please read the comment before changing.
 
   # programs with options
   programs = {
@@ -32,60 +25,54 @@
 #        theme_background = false; #any btop settings break the rebuild for whatever reason
       };
     };
+    kitty = {
+      enable = true;
+      font = {
+        package = [pkgs.nerdfont];
+        name = "JetBrainsMonoNerdFont, Iosevka Nerd Font, Noto Nerd Font";
+        size = "10";
+      };
+    };
   };
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Breeze";
+      package = [pkgs.kdePackages.breeze];
+    };
+    cursorTheme = {
+      name = "bibata-cursors";
+      package = [pkgs.bibata-cursors];
+    };
+    iconTheme = {
+      name = "candy-icons";
+      package = [pkgs.candy-icons];
+    };
+  };
+
+  # The home.packages option allows you to install Nix packages into your environment.
   home.packages = with pkgs; [
     chromium
     thunderbird
     vscodium
     vesktop
     kdePackages.kate
-
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/raumsegler/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    EDITOR = "vscodium";
+    SHELL = "kitty";
   };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+
+  home.stateVersion = "24.11"; # Dont change
 }
