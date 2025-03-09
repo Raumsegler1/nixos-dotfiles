f3b638319b6ec190952e4dfb136f51a84691b14a
Raumsegler1 <113139832+Raumsegler1@users.noreply.github.com.>
2025-03-09 11:56:07 +0100

    temporarily remove old hyprland config

--- modules/hypr.nix
+++ modules/hypr.nix
@@ -5,11 +5,11 @@
     enable = true;
     systemd.variables = ["--all"];
     extraConfig = "
-      ${builtins.readFile ./hypr/hyprland.conf}
-      ${builtins.readFile ./hypr/animations.conf}
-      ${builtins.readFile ./hypr/rules.conf}
-      ${builtins.readFile ./hypr/theme.conf}
-      ${builtins.readFile ./hypr/keybindings.conf}
+#      ${builtins.readFile ./hypr/hyprland.conf}
+#      ${builtins.readFile ./hypr/animations.conf}
+#      ${builtins.readFile ./hypr/rules.conf}
+#      ${builtins.readFile ./hypr/theme.conf}
+#      ${builtins.readFile ./hypr/keybindings.conf}
     ";
   };
 }
