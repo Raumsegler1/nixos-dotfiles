b73ad9f0839b921b98a852f6571c14609fa45ab1
Raumsegler1 <113139832+Raumsegler1@users.noreply.github.com.>
2025-03-10 22:08:09 +0100

    remove asus battery since flake is missing still 77

--- configuration.nix
+++ configuration.nix
@@ -191,11 +191,6 @@
       };
   };
 
-  hardware.asus.battery = {
-  chargeUpto             = 90;   # Maximum level of charge for your battery, as a percentage.
-  enableChargeUptoScript = true; # Whether to add charge-upto to environment.systemPackages. `charge-upto 85` temporarily sets the charge limit to 85%.
-  };
-
   # Enable touchpad support (enabled default in most desktopManager).
   services.libinput.enable = true;
 
