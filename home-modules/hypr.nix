dfd73c6d830de6195b9577edbc2ba0c84117963f
Raumsegler1 <113139832+Raumsegler1@users.noreply.github.com.>
2025-03-12 20:58:54 +0100

    configure hyprland in nix syntax

--- home-modules/hypr.nix
+++ home-modules/hypr.nix
@@ -3,9 +3,11 @@
 {
   wayland.windowManager.hyprland = {
     enable = true;
+    #package = null;
+    #portalPackage = null;
     xwayland.enable = true;
-    systemd.variables = ["--all"];
-/*    extraConfig = ''
+    #systemd.variables = ["--all"];
+    /*extraConfig = ''
       ${builtins.readFile ./hypr/hyprland.conf}
       ${builtins.readFile ./hypr/animations.conf}
       ${builtins.readFile ./hypr/rules.conf}
@@ -18,6 +20,83 @@
 	      "$mod, T, exec, kitty"
 	      "$mod, K, hyprctl disptach exit"
 	      "$mod, C, exec, chromium"
+      ];
+
+      decoration = {
+        rounding = 10;
+        #drop_shadow = false
+        #shadow_range = 1
+        active_opacity = 0.93;
+        inactive_opacity = 0.93;
+        dim_inactive = true;
+        dim_strength = 0.18;
+        dim_special = 0.5;
+        blur = {
+          enabled = yes;
+          size = 5;
+          passes = 3;
+          noise = 0;
+          brightness = 0.90;
+          special = true;
+        };
+      };
+
+      general = {
+        gaps_in = 5;
+        gaps_out = 10;
+        border_size = 3;
+        layout = dwindle;
+        #layout = scroller;
+        resize_on_border = true;
+      };
+
+      dwindle = {
+        pseudotile = no;
+        preserve_split = yes;
+        # special_scale_factor = 0.9;
+      };
+
+      misc = {
+        force_default_wallpaper = 0;
+      };
+
+      input = {
+        kb_layout = de;
+        repeat_rate = 50;
+        repeat_delay = 300;
+
+        follow_mouse = 1;
+        touchpad = {
+          natural_scroll = yes;
+        };
+
+        #sensitivity = -0.3; # -1.0 - 1.0, 0 means no modification.
+        accel_profile = flat;
+      };
+
+      env = [
+        "LIBVA_DRIVER_NAME, nvidia"
+        "__GLX_VENDOR_LIBRARY_NAME, nvidia"
+        #"NVD_BACKEND, direct" # needs libva-nvidia-driver
+        #"WLR_DRM_DEVICES,/dev/dri/card1 # Only use iGPU" # needs working amd-gpu
+
+
+        "XDG_CURRENT_DESKTOP,Hyprland"
+        "XDG_SESSION_TYPE,wayland"
+        "XDG_SESSION_DESKTOP,Hyprland"
+
+        "QT_QPA_PLATFORM,wayland"
+        "QT_QPA_PLATFORMTHEME,qt5ct"
+        "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
+        "QT_AUTO_SCREEN_SCALE_FACTOR,1"
+
+        "MOZ_ENABLE_WAYLAND,1"
+        "WLR_NO_HARDWARE_CURSORS,1"
+      ];
+
+      monitor = [
+        ",preffered,auto,1"
+        "VGA-1,disable"
       ];
     };
   };
